2d297a89967f7ff7656c42bfd90c829e982a0538
rajkumarshivashankar-design <rajkumarshivashankar@gmail.com>
2026-07-26 15:27:17 +0530

    Time: 0 ms (100%), Space: 12.9 MB (13.37%) - LeetHub

--- 0100-same-tree/0100-same-tree.cpp
+++ 0100-same-tree/0100-same-tree.cpp
@@ -15,7 +15,10 @@ public:
         if(p==NULL && q==NULL)return true;
         if(p==NULL || q==NULL)return false;
         if(p->val != q->val)return false;
-        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right); // it will update true or false based when both are true;
+        bool t1=isSameTree(p->left,q->left); 
+        bool t2= isSameTree(p->right,q->right); 
+        
+        return t1 && t2;// it will update true or false based when both are true;
         
     }
 };
