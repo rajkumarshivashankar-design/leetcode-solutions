412d41fbbe0b70665ee2a2799e9872ce4986ed56
Shiva Shankar Rajkumar <rajkumarshivashankar@gmail.com>
2026-08-17 01:43:45 +0530

    Time: 0 ms (100%), Space: 8.3 MB (42.01%) - LeetHub

---
+++ 2351-first-letter-to-appear-twice/2351-first-letter-to-appear-twice.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    char repeatedCharacter(string s) {
+        unordered_map<char,int>f;
+        for(int i=0;i<s.size();i++){
+            f[s[i]]++;
+            if(f[s[i]]==2)return s[i];
+        }
+        return '\0';
+    }
+};
